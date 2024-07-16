7ea3912a9c0fbfa73779bd8ca19a25a71f004a1c
Anibal Reyes <areyes@redabierta.hn>
2024-07-16 11:30:30 -0600

    adding sanity path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,75 +1,72 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 import tailwind from "@astrojs/tailwind";
+import sanity from "@sanity/astro";
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    starlight({
-      title: "FACTURED",
-      description: "Manual de referencia pa emicion de facturas electronicas",
-      favicon: "/favicon.ico",
-      defaultLocale: 'es',
-
-      lastUpdated: true,
-      
-      head: [
-        // Example: add Fathom analytics script tag.
-        {
-          tag: 'script',
-          attrs: {
-            src: 'https://cdn.usefathom.com/script.js',
-            'data-site': 'MY-FATHOM-ID',
-            defer: true,
-          },
-        },
-        {
-          tag:'meta',
-          attrs:{
-            property:'og:locale',
-            content:'es',
-          }
-        }
-      ],
-      customCss: [
-        "./src/styles/globals.css",
-      ],
-      //disable404Route:true,  //Disabling the default 404 page
-      tableOfContents: {
-        minHeadingLevel: 1,
-        maxHeadingLevel: 3,
-      },
-      logo: {
-        replacesTitle: true,
-        dark: "./src/assets/factured_white.svg",
-        light: "./src/assets/factured.svg",
-        
-      },
-      sidebar: [
-        {
-          label: "Inicio",
-          link: "/",
-        },
-        {
-          label: "Guias",
-          collapsed: true,
-
-          items: [
-            // Each item here is one entry in the navigation menu.
-            {
-              label: "Guia de ejemplo",
-              link: "/guides/example/",
-            },
-          ],
-        },
-        {
-          label: "Referencia",
-          collapsed: true,
-          autogenerate: {
-            directory: "reference",
-          },
-        },
-      ],
-    }),
-    tailwind(),
-  ],
+  integrations: [starlight({
+    title: "FACTURED",
+    description: "Manual de referencia pa emicion de facturas electronicas",
+    favicon: "/favicon.ico",
+    defaultLocale: 'es',
+    lastUpdated: true,
+    head: [
+    // Example: add Fathom analytics script tag.
+    {
+      tag: 'script',
+      attrs: {
+        src: 'https://cdn.usefathom.com/script.js',
+        'data-site': 'MY-FATHOM-ID',
+        defer: true
+      }
+    }, {
+      tag: 'meta',
+      attrs: {
+        property: 'og:locale',
+        content: 'es'
+      }
+    }],
+    customCss: ["./src/styles/globals.css"],
+    //disable404Route:true,  //Disabling the default 404 page
+    tableOfContents: {
+      minHeadingLevel: 1,
+      maxHeadingLevel: 3
+    },
+    logo: {
+      replacesTitle: true,
+      dark: "./src/assets/factured_white.svg",
+      light: "./src/assets/factured.svg"
+    },
+    sidebar: [{
+      label: "Inicio",
+      link: "/"
+    }, {
+      label: "Guias",
+      collapsed: true,
+      items: [
+      // Each item here is one entry in the navigation menu.
+      {
+        label: "Guia de ejemplo",
+        link: "/guides/example/"
+      }]
+    }, {
+      label: "Referencia",
+      collapsed: true,
+      autogenerate: {
+        directory: "reference"
+      }
+    }]
+  }),
+  tailwind(),
+  sanity({
+    projectId: 'b1qmda5a',
+    dataset: 'production',
+    apiVersion: '2021-03-25',
+    useCdn: false,
+    studioBasePath: '/admin',
+  }),
+  react()
+]
 });
